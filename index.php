4902ec1d758a87939ea114fbe32c74e2ba7825e4
DuncanAngel <duncanangel070@gmail.com>
2024-12-18 13:36:55 +0100

    Fixed form

--- index.php
+++ index.php
@@ -2,17 +2,15 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $firstName = $_POST["first-name"];
     $lastName = $_POST["last-name"];
-    $orderNumber = $_POST["order-number"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 }
 
-function printMessage($firstName, $lastName, $orderNumber, $email, $message) {
+function printMessage($firstName, $lastName, $email, $message) {
     echo "Success! Your message has been sent. We will get back to you as soon as possible.<br>";
     echo "<strong>Your message:</strong><br>
           <strong>First Name:</strong> $firstName<br>
           <strong>Last Name:</strong> $lastName<br>
-          <strong>Order Number:</strong> $orderNumber<br>
           <strong>Email:</strong> $email<br>
           <strong>Message:</strong> $message";
 }
@@ -25,35 +23,33 @@ function printMessage($firstName, $lastName, $orderNumber, $email, $message) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Review</title>
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<form method="post">
-    <div class="form-floating mb-3">
+<form method="post" class="wrapper">
+    <div class="form-floating">
         <input type="text" class="form-control" name="first-name" id="floatingInput" placeholder="Duncan">
         <label for="floatingInput">First name</label>
-    </div><br>
-    <div class="form-floating mb-3">
+    </div>
+    <div class="form-floating">
         <input type="text" class="form-control" name="last-name" id="floatingInput" placeholder="Jansen">
         <label for="floatingInput">Last name</label>
-    </div><br>
-    <input type="number" name="order-number" placeholder="Order number"><br>
-    <div class="form-floating mb-3">
+    </div>
+    <div class="form-floating">
         <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
         <label for="floatingInput">Email address</label>
-    </div><br>
-    <div class="mb-3">
-        <label for="textArea" class="form-label">Add message...</label>
-        <textarea class="form-control" id="textArea" name="message" rows="3"></textarea>
-    </div><br>
+    </div>
+    <div>
+        <textarea class="form-floating" id="textArea" name="message" rows="3" placeholder="Add message..."></textarea>
+    </div>
     <button class="btn btn-primary" name="submit" type="submit">Submit form</button>
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php
-if (isset($_POST['submit'])) {
-    printMessage($firstName, $lastName, $orderNumber, $email, $message);
-}
-
-?>
+//if (isset($_POST['submit'])) {
+//    printMessage($firstName, $lastName, $orderNumber, $email, $message);
+//}
+//?>
